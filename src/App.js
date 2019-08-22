cfa0b25d15e5149fc3a467c94d466a323064d900
alexiliev <iliev.alex@outlook.com>
2019-08-22 15:30:40 -0700

    adding Carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import './App.css';
+import Carousel from './components/Carousel'
+import { render } from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
      <NavBar />
+     <Carousel />
     </div>
   );
 }
---
+++ src/components/Carousel.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+
+export default () => (
+  <Carousel autoPlay>
+    <div>
+      <img src='https://i.imgur.com/nHCTdCk.jpg' />
+      <p className='legend'>Legend 1</p>
+    </div>
+    <div>
+      <img src='https://i.imgur.com/1QaCXCz.jpg' />
+      <p className='legend'>Legend 2</p>
+    </div>
+    <div>
+      <img src='https://i.imgur.com/EqgIxXu.jpg' />
+      <p className='legend'>Legend 3</p>
+    </div>
+    <div>
+      <img src='https://i.imgur.com/WVil30p.jpg' />
+      <p className='legend'>Legend 4</p>
+    </div>
+    
+  </Carousel>
+)
