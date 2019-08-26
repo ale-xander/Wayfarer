3f04648428feb10de7fa6ca5ef0efd50a0f6b1cb
nabilnassery <qbmd19@gmail.com>
2019-08-26 15:05:06 -0700

    fixed landing page, carousel, popular cities, and navbar

--- src/components/Landing/PopularCities.js
+++ src/components/Landing/PopularCities.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const PopularCities = () => {
     return (
-        <>
-        
         <div className="container"> 
             <div className="city1 city-style">
             <h1>San Francisco</h1>
@@ -18,19 +16,6 @@ const PopularCities = () => {
             <p>Cabo San Lucas, a resort city on the southern tip of Mexico’s Baja California peninsula, is known for its beaches, water-based activities and nightlife. Playa El Médano is Cabo’s main beach, with outdoor restaurants and numerous bars. Past the marina is Land's End promontory, site of Playa del Amor  and El Arco, a natural archway in the seacliffs.</p>
             </div>
         </div>
-        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-  <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-  <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-  <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
-</div>
-<div class="tab-content" id="v-pills-tabContent">
-  <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-  <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-  <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-</div>
-</>
     );
 };
 
