d5064bbcd697b3f4a996183e5d3e9396d2aa3637
alexiliev <iliev.alex@outlook.com>
2019-08-30 17:44:00 -0700

    puhsing to heroku

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4000/api/v1';
-//export const API_URL = ' https://wayfarer-back-end.herokuapp.com';
+//export const API_URL = 'http://localhost:4000/api/v1';
+export const API_URL = 'https://wayfarer-back-end.herokuapp.com';
